f6ca8e2fd6fa9a9ca0d3f769b565b99311152f0f
ujjwal05062002 <ujjwal05062002@gmail.com>
2025-03-13 04:22:01 +0530

    basic strucutre done

--- newsapp/src/ClassComponent/App.jsx
+++ newsapp/src/ClassComponent/App.jsx
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Navbar from './Navbar';
+import Footer from './Footer';
+import Home from './Home'
 
 export default class App extends Component {
   render() {
     return (
       <>
-        
+        <BrowserRouter>
+        <Navbar />
+        <Routes>
+            <Route path='/' element={<Home />}  />
+        </Routes>
+        <Footer />
+        </BrowserRouter>
       </>
     );
   }
