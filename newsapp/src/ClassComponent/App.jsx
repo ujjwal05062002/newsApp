ed8d1b403af995449e86e7a4120294cca6729dd7
ujjwal05062002 <ujjwal05062002@gmail.com>
2025-03-13 04:18:23 +0530

    out of memory error occured

--- newsapp/src/ClassComponent/App.jsx
+++ newsapp/src/ClassComponent/App.jsx
@@ -1,22 +1,11 @@
-import React from "react";
-import { Component } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Navbar from "./Navbar";
-import Footer from "./Footer";
-import Home from "./Home";
-
+import React, { Component } from 'react';
 
 export default class App extends Component {
   render() {
     return (
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          
-        </Routes>
-        <Footer />
-      </BrowserRouter>
+      <>
+        
+      </>
     );
   }
 }
--- newsapp/src/ClassComponent/Footer.jsx
+++ newsapp/src/ClassComponent/Footer.jsx
@@ -3,11 +3,9 @@ import React, { Component } from 'react';
 export default class Footer extends Component {
   render() {
     return (
-      <>
-       <Footer className='bg-secondary text-center text-light p-2 mt-1'>
-          <p>copyright@newsapp.ujjwal.com</p>
-        </Footer>   
-      </>
+      <div>
+        
+      </div>
     );
   }
 }
