5557092a8224e667b8ccb96b7edb363d6b45408f
ujjwal05062002 <ujjwal05062002@gmail.com>
2025-03-15 03:09:14 +0530

    concatination added

--- newsapp/src/ClassComponent/Home.jsx
+++ newsapp/src/ClassComponent/Home.jsx
@@ -1,25 +1,39 @@
 import React, { Component } from 'react';
 import NewsItem from './NewsItem';
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 export default class Home extends Component {
     constructor() {
         super()
         this.state = {
             totalResults: 0,
-            articles: []
+            articles: [],
+            page: 1,
+            q: ""
         }
     }
 
     async getAPIData(q) {
-        var response = await fetch(`https://newsapi.org/v2/everything?q=${q}&language=${this.props.language}&sortBy=publishedAt&apiKey=9a57fe8c40e64e4da0eddcbe81c2d6f3`)
+        this.setState({ page: 1, q: q })
+        var response = await fetch(`https://newsapi.org/v2/everything?q=${q}&page=${this.state.page}&pageSize=12&language=${this.props.language}&sortBy=publishedAt&apiKey=9a57fe8c40e64e4da0eddcbe81c2d6f3`)
         response = await response.json()
         this.setState({
             totalResults: response.totalResults,
-            articles: response.articles
+            articles: response.articles.filter((x) => x.title !== "[Removed]")
         })
     }
 
-    
+    fetchData = async () => {
+        this.setState({ page: this.state.page + 1 })
+        var response = await fetch(`https://newsapi.org/v2/everything?q=${this.state.q}&page=${this.state.page}&pageSize=12&language=${this.props.language}&sortBy=publishedAt&apiKey=9a57fe8c40e64e4da0eddcbe81c2d6f3`)
+        response = await response.json()
+        if (response.articles) {
+            this.setState({
+                totalResults: response.totalResults,
+                articles: this.state.articles.concat(response.articles.filter((x) => x.title !== "[Removed]"))
+            })
+        }
+    }
 
     componentDidMount() {
         this.getAPIData(this.props.q)
@@ -38,22 +52,34 @@ export default class Home extends Component {
             <>
                 <div className="container-fluid my-3">
                     <h4 className='bg-secondary text-light p-2 text-center'>{this.props.q} News Items</h4>
-                    <div className="row">
-                        {
-                            this.state.articles.map((item, index) => {
-                                return <NewsItem
-                                    key={index}
-                                    title={item.title}
-                                    description={item.description}
-                                    source={item.source.name}
-                                    url={item.url}
-                                    pic={item.urlToImage}
-                                    date={(new Date(item.publishedAt)).toLocaleDateString()}
-                                />
-                            })
+                    <InfiniteScroll
+                        dataLength={this.state.articles.length}
+                        next={this.fetchData}
+                        hasMore={this.state.articles.length < this.state.totalResults}
+                        loader={<h4>Loading...</h4>}
+                        endMessage={
+                            <p style={{ textAlign: 'center' }}>
+                                <b>No More News Articles</b>
+                            </p>
                         }
-                    </div>
 
+                    >
+                        <div className="row">
+                            {
+                                this.state.articles.map((item, index) => {
+                                    return <NewsItem
+                                        key={index}
+                                        title={item.title}
+                                        description={item.description}
+                                        source={item.source.name}
+                                        url={item.url}
+                                        pic={item.urlToImage}
+                                        date={(new Date(item.publishedAt)).toLocaleDateString()}
+                                    />
+                                })
+                            }
+                        </div>
+                    </InfiniteScroll>
                 </div>
             </>
         );
